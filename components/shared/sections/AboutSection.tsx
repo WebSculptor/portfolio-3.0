1dc1d15da976f0ac497bf95c200d418b802794e2
Abdullahi Salihu <131313531+WebSculptor@users.noreply.github.com>
2024-03-17 07:07:35 +0100

    Update AboutSection.tsx

--- components/shared/sections/AboutSection.tsx
+++ components/shared/sections/AboutSection.tsx
@@ -13,13 +13,13 @@ export default function AboutSection() {
 
       <div className="flex flex-col gap-3 flex-1">
         <p className="text-xs md:text-sm leading-7 font-normal text-muted-foreground">
-          Hello, I&apos;m Abdullahi Salihu, a front-end developer who loves to create visually striking user interfaces. I create cutting-edge web applications that skillfully combine functionality and design, with a particular emphasis on technologies like React, Next.js, and TypeScript.
+           👋 Hi there, I&apos;m a front-end developer who loves to create visually striking user interfaces. I create cutting-edge web applications that skillfully combine functionality and design, with a particular emphasis on technologies like React, Next.js, TypeScript and Tailwind.
         </p>
         <p className="text-xs md:text-sm leading-7 font-normal text-muted-foreground">
-         I enjoy working with teams to surpass project goals and converting intricate concepts into beautiful code. I enjoy the challenge of finding solutions and pushing the frontiers of technology, whether I&apos;m using Solidity to explore blockchain integration or SCSS to create engaging interfaces. Aside from coding, I like going on outdoor excursions, taking pictures, and always improving both personally and professionally.
+           I enjoy working with teams to surpass project goals, converting intricate concepts into beautiful code, the challenge of finding solutions and pushing the frontiers of technology, whether I&apos;m using Solidity to explore blockchain integration or SCSS to create engaging interfaces. Aside from coding, I like going on outdoor excursions, taking pictures, and always improving both personally and professionally.
         </p>
         <p className="text-xs md:text-sm leading-7 font-normal text-muted-foreground">
-           I&apos;m always up for a conversation, so let&apos;s connect and make something amazing together!
+           Let&apos;s connect and create something extraordinary together. Feel free to reach out—I&apos;d love to chat!
         </p>
       </div>
     </MaxContainer>
