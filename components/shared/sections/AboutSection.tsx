c2e479c59580a5f112e12c51e1b99216d807c277
Abdullahi Salihu <131313531+WebSculptor@users.noreply.github.com>
2024-03-17 07:02:30 +0100

    Update AboutSection.tsx

--- components/shared/sections/AboutSection.tsx
+++ components/shared/sections/AboutSection.tsx
@@ -12,13 +12,13 @@ export default function AboutSection() {
       </div>
 
       <div className="flex flex-col gap-3 flex-1">
-        <p className="text-sm leading-7 font-normal text-muted-foreground">
+        <p className="text-xs md:text-sm leading-7 font-normal text-muted-foreground">
           Hello, I&apos;m Abdullahi Salihu, a front-end developer who loves to create visually striking user interfaces. I create cutting-edge web applications that skillfully combine functionality and design, with a particular emphasis on technologies like React, Next.js, and TypeScript.
         </p>
-        <p className="text-sm leading-7 font-normal text-muted-foreground">
+        <p className="text-xs md:text-sm leading-7 font-normal text-muted-foreground">
          I enjoy working with teams to surpass project goals and converting intricate concepts into beautiful code. I enjoy the challenge of finding solutions and pushing the frontiers of technology, whether I&apos;m using Solidity to explore blockchain integration or SCSS to create engaging interfaces. Aside from coding, I like going on outdoor excursions, taking pictures, and always improving both personally and professionally.
         </p>
-        <p className="text-sm leading-7 font-normal text-muted-foreground">
+        <p className="text-xs md:text-sm leading-7 font-normal text-muted-foreground">
            I&apos;m always up for a conversation, so let&apos;s connect and make something amazing together!
         </p>
       </div>
